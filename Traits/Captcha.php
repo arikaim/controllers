a5e392a8db0fea7a3f6dd95bc64564bc2e6effa0
Arikaim <sales@intersoft.biz>
2020-02-03 18:12:42 +0200

    =

---
+++ Traits/Captcha.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c)  Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Core\Controllers\Traits;
+
+/**
+ * Captcha trait
+*/
+trait Captcha 
+{        
+     /**
+     * Verify captcha
+     *   
+     * @param \Psr\Http\Message\ServerRequestInterface $request    
+     * @param Validator $data
+     * @return boolean
+     */
+    public function verifyCaptcha($request, $data)
+    {
+        $current = $this->get('options')->get('captcha.current');
+        $recaptcha = $this->get('driver')->create($current);
+
+        $result = $recaptcha->verify($data['g-recaptcha-response'],$request->getAttribute('client_ip'));        
+        if ($result == false) {
+            $this->error('errors.captcha');      
+        }   
+        
+        return $result;
+    }
+}
