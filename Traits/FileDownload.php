a77cb8208174d289a79c55475fc4250bf7e705db
Arikaim <sales@intersoft.biz>
2020-03-05 15:28:47 +0200

    =

--- Traits/FileDownload.php
+++ Traits/FileDownload.php
@@ -9,21 +9,26 @@
 */
 namespace Arikaim\Core\Controllers\Traits;
 
+use GuzzleHttp\Psr7;
+use Psr\Http\Message\StreamInterface;
+
 /**
- * File download trait
+ * File download and image view trait
 */
 trait FileDownload 
 {            
     /**
-     * Set response headers
+     * Download file
      *
-     * @param object $response
+     * @param \Psr\Http\Message\ResponseInterface $response
      * @param string $fileName
-     * @param \Slim\Http\Stream $stream
-     * @return object
+     * @param \Psr\Http\Message\StreamInterface|string $stream
+     * @return \Psr\Http\Message\ResponseInterface
      */
     public function downloadFileHeaders($response, $fileName, $stream)
     {
+        $stream = ($stream instanceof StreamInterface) ? $stream : Psr7\stream_for($stream);
+
         return $response
             ->withHeader('Content-Type','application/force-download')
             ->withHeader('Content-Type','application/octet-stream')
@@ -35,5 +40,63 @@ trait FileDownload
             ->withHeader('Cache-Control','must-revalidate, post-check=0, pre-check=0')
             ->withHeader('Pragma','public')
             ->withBody($stream); 
-    }    
+    } 
+
+    /**
+     * Download file
+     *
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param string $filePath  
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function downloadFile($response, $filePath)
+    {
+        if ($this->get('storage')->has($filePath) == true) {
+            $data = $this->get('storage')->read($filePath);  
+            $fileName = basename($filePath);        
+        } 
+
+        return $this->downloadFileHeaders($response,$fileName,$data);
+    } 
+
+    /**
+     * View image headers
+     *
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param string $type Content type
+     * @param  \Psr\Http\Message\StreamInterface|string $stream
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function viewImageHeaders($response, $type, $stream)
+    {
+        $stream = ($stream instanceof StreamInterface) ? $stream : Psr7\stream_for($stream);
+
+        return $response
+            ->withHeader('Content-Type',$type)                      
+            ->withHeader('Expires','0')
+            ->withHeader('Cache-Control','must-revalidate, post-check=0, pre-check=0')
+            ->withHeader('Pragma','public')
+            ->withBody($stream); 
+    }
+
+    /**
+     * View image
+     *
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param string $imagePath 
+     * @param string|null $imgeNotFoundPath 
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function viewImage($response, $imagePath, $imgeNotFoundPath = null)
+    {
+        if ($this->get('storage')->has($imagePath) == true) {
+            $data = $this->get('storage')->read($imagePath);
+            $type = $this->get('storage')->getMimetype($imagePath);
+        } else {
+            $data = $this->get('storage')->read($imgeNotFoundPath);
+            $type = $this->get('storage')->getMimetype($imgeNotFoundPath); 
+        }
+        
+        return $this->viewImageHeaders($response,$type,$data);
+    }
 }
